9dafca7ade1021915534860d1e480b2fae3469f5
Dimon <notawid@gmail.com>
2024-07-31 10:50:33 +0200

    Added Google section

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import "../styles/reset.css";
-import "../styles/general.css";
+import "../styles/global.css";
 import "../styles/header.css";
 import "../styles/salon-employees.css";
 import "../styles/booksy.css";
 import "../styles/gallery.css";
 import "../styles/pricelist.css";
+import "../styles/google.css";
 import "../styles/footer.css";
 
 import { priceTabs, deactivateTabs, renderPriceList, resizePriceTabs } from "./priceList.js";
@@ -14,6 +15,7 @@ const menuIcon = document.querySelector('.menu');
 const navigationBar = document.querySelector('nav');
 let isHidden = true;
 const booksyButton = document.querySelector('.booksy-anchor');
+const googleButton = document.querySelector('.google-anchor');
 const toggleButton = document.querySelector('.toggle-button');
 const theme = localStorage.getItem('theme');
 
@@ -45,15 +47,26 @@ menuIcon.addEventListener('click', () => {
   isHidden ? showMenu() : hideMenu()
 })
 
-navigationBar.addEventListener('mouseover', showMenu)
-navigationBar.addEventListener('mouseout', hideMenu)
+navigationBar.addEventListener('mouseover', showMenu);
+navigationBar.addEventListener('mouseout', hideMenu);
 navigationBar.addEventListener('click', (event) => {
   if(event.target.tagName != 'IMG') { hideMenu() }
 })
 
+
 booksyButton.addEventListener('click', () => {
   window.open('https://booksy.com/pl-pl/228333_house-of-nails_paznokcie_20383_gdansk', '_blank')
 })
+
+googleButton.addEventListener('mouseover', () => googleButtonHandle(true));
+googleButton.addEventListener('mouseout', () => googleButtonHandle(false));
+googleButton.addEventListener('touchstart', () => googleButtonHandle(true));
+googleButton.addEventListener('touchend', () => googleButtonHandle(false));
+
+googleButton.addEventListener('click', () => {
+  window.open('https://g.page/r/CfMB2T9FtuLoEBE/review', '_blank')
+})
+
 
 priceTabs.forEach((tab) => {
   tab.addEventListener('click', () => {
@@ -73,6 +86,15 @@ function showMenu() {
 function hideMenu() {
   navigationBar.style.visibility = 'hidden';
   isHidden = true;
+}
+
+
+function googleButtonHandle(hover) {
+  if(hover === true) {
+    googleButton.src = '/google-hover.avif';
+  } else {
+    googleButton.src = '/google.avif';
+  }
 }
 
 
